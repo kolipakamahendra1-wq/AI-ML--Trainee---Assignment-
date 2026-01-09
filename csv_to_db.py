99be7c263b7f3b522bbb9a9e6063e859887803c0
kolipakamahendra1-wq <kolipakamahendra1@gmail.com>
2026-01-09 12:22:02 +0530

    Update csv_to_db.py

--- csv_to_db.py
+++ csv_to_db.py
@@ -1,1 +1,24 @@
+import csv
+import sqlite3
 
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT
+)
+""")
+
+with open("users.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        cursor.execute(
+            "INSERT INTO users (name, email) VALUES (?, ?)",
+            (row["name"], row["email"])
+        )
+
+conn.commit()
+conn.close()
