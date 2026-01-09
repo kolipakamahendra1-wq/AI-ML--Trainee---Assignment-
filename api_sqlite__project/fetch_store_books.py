08f16e42b8767c35f50a4d83b50b4b7c07842a86
kolipakamahendra1-wq <kolipakamahendra1@gmail.com>
2026-01-09 11:59:45 +0530

    Update fetch_store_books.py

--- api_sqlite__project/fetch_store_books.py
+++ api_sqlite__project/fetch_store_books.py
@@ -1,1 +1,33 @@
+import requests
+import sqlite3
 
+API_URL = "https://fakerapi.it/api/v1/books?_quantity=10"
+
+response = requests.get(API_URL)
+books = response.json()["data"]
+
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    author TEXT,
+    year INTEGER
+)
+""")
+
+for book in books:
+    cursor.execute(
+        "INSERT INTO books (title, author, year) VALUES (?, ?, ?)",
+        (book["title"], book["author"], book["published"])
+    )
+
+conn.commit()
+
+cursor.execute("SELECT * FROM books")
+for row in cursor.fetchall():
+    print(row)
+
+conn.close()
