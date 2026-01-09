b97c57c1b53b68adb3515bdd5885e6d2c81a97ce
kolipakamahendra1-wq <kolipakamahendra1@gmail.com>
2026-01-09 12:13:55 +0530

    Update student_scores.py

--- student_scores.py
+++ student_scores.py
@@ -1,1 +1,20 @@
+import matplotlib.pyplot as plt
 
+students = [
+    {"name": "Asha", "score": 78},
+    {"name": "Ravi", "score": 85},
+    {"name": "Neha", "score": 92},
+    {"name": "Arjun", "score": 70}
+]
+
+names = [s["name"] for s in students]
+scores = [s["score"] for s in students]
+
+average = sum(scores) / len(scores)
+print("Average Score:", average)
+
+plt.bar(names, scores)
+plt.xlabel("Students")
+plt.ylabel("Scores")
+plt.title("Student Test Scores")
+plt.show()
